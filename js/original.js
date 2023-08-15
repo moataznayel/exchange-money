5d7344a9fdf537cb1ab57e4dad0382de6f3fdd57
moataz <moataznayel458@gmail.com.com>
2023-08-15 06:38:02 +0300

    edit in style css

--- js/original.js
+++ js/original.js
@@ -1,41 +1,54 @@
-let mySelect1 = document.querySelector(".form");
-let mySelect = document.querySelector(".select");
+let mySelectFrom = document.querySelector(".select-from");
+let mySelectTo = document.querySelector(".select-to");
 let myInput = document.querySelector(".form-control");
 let myBtn = document.querySelector(".btn");
 let myChange = document.querySelector(".change");
+let exchange = document.querySelector("i");
 myInput.value = 1;
 
-fetch("https://v6.exchangerate-api.com/v6/e81264b84019ea57a88fed63/latest/USD")
+fetch(`https://v6.exchangerate-api.com/v6/e81264b84019ea57a88fed63/latest/usd`)
   .then((r) => r.json())
   .then((data) => {
+    console.log(data.conversion_rates.EGP);
     for (let ele in data.conversion_rates) {
       let myOption = `<option>${ele}</option>`;
-      mySelect1.innerHTML += myOption;
-      mySelect.innerHTML += myOption;
+      mySelectFrom.innerHTML += myOption;
+      mySelectTo.innerHTML += myOption;
     }
-    mySelect.value = "EGP";
-    fetch(
-      ` https://v6.exchangerate-api.com/v6/e81264b84019ea57a88fed63/latest/${mySelect1.value}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        mySelect1.addEventListener("change", () => mySelect1.value);
-        mySelect.addEventListener("change", () => mySelect.value);
-        let result =
-          myInput.value *
-          data.conversion_rates[mySelect1.value] *
-          data.conversion_rates[mySelect.value];
-        myChange.innerHTML = `${myInput.value} ${
-          mySelect1.value
-        } = ${result.toFixed(2)} ${mySelect.value}`;
-        myBtn.addEventListener("click", () => {
-          let result =
-            myInput.value *
-            data.conversion_rates[mySelect1.value] *
-            data.conversion_rates[mySelect.value];
-          myChange.innerHTML = `${myInput.value} ${
-            mySelect1.value
-          } = ${result.toFixed(2)} ${mySelect.value}`;
-        });
-      });
+    mySelectTo.value = "EGP";
+    myChange.innerHTML = `1 USD = ${data.conversion_rates.EGP.toFixed(2)} ${
+      mySelectTo.value
+    }`;
   });
+
+myBtn.addEventListener("click", () => {
+  fetch(
+    ` https://v6.exchangerate-api.com/v6/e81264b84019ea57a88fed63/latest/${mySelectFrom.value}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      myChange.innerHTML = `${parseInt(myInput.value)} ${
+        mySelectFrom.value
+      } = ${(
+        data.conversion_rates[mySelectTo.value] * parseInt(myInput.value)
+      ).toFixed(2)} ${mySelectTo.value}`;
+    });
+});
+
+exchange.addEventListener("click", () => {
+  let to = mySelectTo.value;
+  let from = mySelectFrom.value;
+  mySelectFrom.value = to;
+  mySelectTo.value = from;
+  fetch(
+    ` https://v6.exchangerate-api.com/v6/e81264b84019ea57a88fed63/latest/${mySelectFrom.value}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      myChange.innerHTML = `${parseInt(myInput.value)} ${
+        mySelectFrom.value
+      } = ${(
+        data.conversion_rates[mySelectTo.value] * parseInt(myInput.value)
+      ).toFixed(2)} ${mySelectTo.value}`;
+    });
+});
